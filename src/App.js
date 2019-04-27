96ba20257d65913be6445c7da5b9dfaee62c2ecc
Sieger <sieger@siegers-mbp.attlocal.net>
2019-04-27 10:39:51 -0500

    Barebones React App

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
+
+var config = {
+  apiKey: "AIzaSyDEYL19DU1nZnv71Xusq5qzFEs83lpO7Wo",
+  authDomain: "bloc-chat-50e68.firebaseapp.com",
+  databaseURL: "https://bloc-chat-50e68.firebaseio.com",
+  projectId: "bloc-chat-50e68",
+  storageBucket: "bloc-chat-50e68.appspot.com",
+  messagingSenderId: "294343061556"
+};
+firebase.initializeApp(config);
+
+class App extends Component {
+  render() {
+    return (
+     true
+    );
+  }
+}
+
+export default App;
+
